9b4a28050d9272e319ceb340158310d22f29d422
Alex Leversen <adleversen@sep.com>
2018-02-11 20:48:38 +0000

    Added functionality for up to 3 drives, eliminated threading issues.

--- floppotron.py
+++ floppotron.py
@@ -1,78 +1,68 @@
+#!/usr/bin/env python3
 import usb.core
 import usb.util
 import array
 import RPi.GPIO as GPIO
-from threading import Timer
+import threading
 import time
 
 headPos = 0
 delay = 0
+DRIVEMAP = [[17,18],[22,23],[26,20]]
+delays = [0,0,0]
+notes = [0,0,0]
 
 def calcFreq(keyPressed):
 	return 55 * (2 ** ((keyPressed - 57) / 12))
 
-def startTimer():
-	global headPos
-	global delay
-	if(delay == 0):
-		return
-	t = Timer(delay,startTimer)
-	if(GPIO.input(5)):
-		headPos = headPos + 1
-		if(headPos == 70):
-			GPIO.output(5,0)
-	else:
-		headPos = headPos - 1
-		if(headPos == 0):
-			GPIO.output(5,1)
-	t.start()
-	return
-
-def playNote(data, device):
-	global delay
-	freq = calcFreq(float(data[2]))
-	delay = 1/freq
-	notes = [delay]
-	p = GPIO.PWM(3,freq)
-	p.start(50)
-	startTimer()
+def runDrive(driveNum):
+	global delays
+	direct = DRIVEMAP[driveNum][0]
+	stp = DRIVEMAP[driveNum][1]
 	while(True):
-		d = device.read(0x82, 0x4)
-		if(d[0] == 0x9):
-			if(d[3] == 0x0):
-				notes.remove(1/calcFreq(float(d[2])))
-				if(len(notes) == 0):
-					delay = 0
-					p.stop()
-					return
-				else:
-					delay = notes[len(notes)-1]
-			else:
-				freq = calcFreq(float(d[2]))
-				delay = 1/freq
-				notes.append(delay)
-				p.stop()
-				p = GPIO.PWM(3,freq)
-				p.start(50)
-				startTimer()
+		if(delays[driveNum] == 0):
+			time.sleep(0.05)
+		else:
+			GPIO.output(direct, not GPIO.input(direct))
+			GPIO.output(stp, 1)
+			time.sleep(delays[driveNum]/2)
+			GPIO.output(stp, 0)
+			time.sleep(delays[driveNum]/2)
+	
+def playNote(data):
+	global delays, notes
+	if(data[3] == 0):
+		if data[2] in notes:
+			n = notes.index(data[2])
+			notes[n] = 0
+			delays[n] = 0
+	else:
+		if 0 in notes:
+			n = notes.index(0)
+			notes[n] = data[2]
+			delays[n] = 1/calcFreq(data[2])
 		
 				
 
 def main():
+	global delays
+	GPIO.setmode(GPIO.BCM)
+	for d,s in DRIVEMAP:
+		GPIO.setup(d, GPIO.OUT)
+		GPIO.setup(s, GPIO.OUT)
+		GPIO.output(d,1)
+	t0 = threading.Thread(target=runDrive, args=[0], daemon=True)
+	t0.start()
+	t1 = threading.Thread(target=runDrive, args=[1], daemon=True)
+	t1.start()
+	t2 = threading.Thread(target=runDrive, args=[2], daemon=True)
+	t2.start()
 	device = usb.core.find(idVendor=0x0499, idProduct=0x1030)
 	try:
 		device.detach_kernel_driver(0)
 	except:
 		pass
 	endpoint = device[0][(0,0)][0]
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(3, GPIO.OUT)
-	GPIO.setup(5, GPIO.OUT)
-	GPIO.output(5,1)
-	p = GPIO.PWM(3,500)
-	p.start(50)
-	time.sleep(0.5)
-	p.stop()
 
 	data = None
 	while True:
@@ -80,7 +70,7 @@ def main():
 			data = device.read(0x82, 0x4)
 			if(data[0] == 0x9):
 				print("playing note " + str(data[2]))
-				playNote(data, device)
+				playNote(data)
 		except usb.core.USBError as e:
 			data = None
 			if e.args == ('Operation timed out',):
